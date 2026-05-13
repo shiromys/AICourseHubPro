54f1c3c52af536b3daf13e567d75bbb5b7347412
Akash Vijaykumar <akashwodeyar91@gmail.com>
2026-05-13 14:59:20 +0530

    added custome domain to backend service - updating the config.js file henceforth

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // frontend/src/config.js
 // API base URL - points to Flask backend on Railway
-const API_BASE_URL = "https://aicoursehubpro-production.up.railway.app";
+const API_BASE_URL = "https://api.aicoursehubpro.com";
 
 export default API_BASE_URL;
--- frontend/src/dist/config.dev.js
+++ frontend/src/dist/config.dev.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 // frontend/src/config.js
 // API base URL - points to Flask backend on Railway
-var API_BASE_URL = "https://aicoursehubpro-production.up.railway.app";
+var API_BASE_URL = "https://api.aicoursehubpro.com";
 var _default = API_BASE_URL;
 exports["default"] = _default;
 //# sourceMappingURL=config.dev.js.map
